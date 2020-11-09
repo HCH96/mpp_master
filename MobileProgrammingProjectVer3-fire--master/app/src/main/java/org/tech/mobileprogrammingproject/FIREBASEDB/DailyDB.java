bab3041e2d636603293620d412c624e1360ca1d4
jiwon-33 <71117552+jiwon-33@users.noreply.github.com>
2020-11-10 06:48:14 +0900

    Add files via upload

--- MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/FIREBASEDB/DailyDB.java
+++ MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/FIREBASEDB/DailyDB.java
@@ -9,31 +9,39 @@ public class DailyDB {
     public String content;
     public int state;
     public long date;
-    public long startTime;
-    public long endTime;
+    public int startTime;
+    public int endTime;
+    public String time;
     public String catalog;
+    public String createDate;
+    public int timeline;
 
     public DailyDB(){
 
     }
 
-    public DailyDB(Date createDate, String content, int state, long date, long startTime, long endTime, String catalog){
+    public DailyDB(String createDate, String content, int state, long date, int startTime, int endTime, String time, String catalog, int timeline){
+        this.createDate = createDate;
         this.content = content;
+        this.timeline = timeline;
         this.state = state;
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
         this.catalog = catalog;
+        this.time = time;
     }
 
     public Map<String, Object> toMap(){
         HashMap<String, Object> result = new HashMap<>();
+        result.put("createDate", createDate);
         result.put("content", content);
         result.put("state", state);
         result.put("date", date);
         result.put("startTime", startTime);
         result.put("endTime", endTime);
         result.put("catalog", catalog);
+        result.put("time", time);
         return result;
     }
 }
