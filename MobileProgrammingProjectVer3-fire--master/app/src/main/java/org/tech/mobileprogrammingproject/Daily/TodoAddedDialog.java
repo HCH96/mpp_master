20c5c30851cff9d877b5608852a35cc3df77ed81
jiwon-33 <71117552+jiwon-33@users.noreply.github.com>
2020-11-14 05:56:44 +0900

    Add files via upload

--- MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/Daily/TodoAddedDialog.java
+++ MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/Daily/TodoAddedDialog.java
@@ -37,28 +37,35 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ * TodoAddedDialog.java
+ * 주요 기능: 할 일 추가 팝업
+ *
+ * 2020.11.14 (마지막 수정)
+ * @author 김지원
+ */
+
 public class TodoAddedDialog extends DialogFragment implements View.OnClickListener {
 
-    /*
-    1. variables :
-        bt_cancel : (등록) 취소 버튼
-        bt_listUp : 할 일 추가 버튼
-        et_todo : dialog에서 입력받은 할 일(EditText)
-    2. functions:
-*/
+    // variables
+    private Button bt_cancel; // 등록 취소 버튼
+    private Button bt_listUp; // 할 일 등록 버튼
+    private RadioGroup timeGroup; // 3분할 설정 RadioGroup
+    private ImageButton delBtn; // 삭제 버튼
+    EditText content; // 할 일 입력 EditText
+    Spinner spinner; // 카테고리 설정 spinner
 
-    private Button bt_cancel;
-    private Button bt_listUp;
+    // firebase 연결 및 DB 변수
     DatabaseReference database = null;
-    private RadioGroup timeGroup;
-    private LinearLayout todolist;
-    Spinner spinner;
     DailyDB dailydb = null;
-    Calendar cal;
-    EditText content;
-    ImageButton delBtn;
+
+
+    // 카테고리 이름을 담기 위한 String ArrayList
     ArrayList<String> list = new ArrayList<>();
 
+    Calendar cal;
+
+    // 새로운 TodoAddedDialog 생성 및 return
     public static final String TAG_EVENT_DIALOG = "dialog_event";
     public static TodoAddedDialog getInstance() {
         TodoAddedDialog e = new TodoAddedDialog();
@@ -69,18 +76,26 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        // firebase 연결
         database = FirebaseDatabase.getInstance().getReference();
+
+        // 신규 생성 시
         if(getArguments().getInt("state") == 0) {
+            // todo_popup layout과 spinner layout 받아옴
             View v = inflater.inflate(R.layout.todo_popup, container, false);
             spinner = (Spinner) v.findViewById(R.id.category_spinner);
 
+            // 사용자 카테고리 생성을 위해 addValueEventListener 구현
             database.child("category").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    // 추가된 카테고리가 없을 경우 기본 카테고리 제공
                     if(!snapshot.exists()){
                         list.add("미정");
                         list.add("공부");
                         list.add("과제");
+                        list.add("운동");
                     }else {
                         for (DataSnapshot childData : snapshot.getChildren()) {
                             categoryDB currData = childData.getValue(categoryDB.class);
@@ -95,36 +110,38 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
                 }
             });
 
-            //ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(v.getContext(), android.R.layout.simple_spinner_item, list);
-            //spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            //spinner.setAdapter(spinnerAdapter);
             ArrayAdapter<CharSequence> adapterArray = ArrayAdapter.createFromResource(v.getContext(), R.array.category_list, android.R.layout.simple_spinner_item);
             adapterArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(adapterArray);
             cal = Calendar.getInstance();
-            content = v.findViewById(R.id.add_todo);
-            database = FirebaseDatabase.getInstance().getReference();
 
+            // layout과 button 연결
+            content = v.findViewById(R.id.add_todo);
             bt_cancel = v.findViewById(R.id.bt_cancel);
             bt_listUp = v.findViewById(R.id.bt_listUp);
-
-            todolist = v.findViewById(R.id.todolist);
             timeGroup = v.findViewById(R.id.time_group);
+
+            // 구현해둔 OnClick 함수 연결
             bt_cancel.setOnClickListener(this);
             bt_listUp.setOnClickListener(this);
             setCancelable(false);
             return v;
-        }else{
+
+        } else { // 기존 할 일 수정
+            // todo_popup layout과 spinner layout 받아옴
             View v = inflater.inflate(R.layout.todo_popup, container, false);
             spinner = (Spinner) v.findViewById(R.id.category_spinner);
 
+            // 사용자 카테고리 생성을 위해 addValueEventListener 구현
             database.child("category").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    // 추가된 카테고리가 없을 경우 기본 카테고리 제공
                     if(!snapshot.exists()){
                         list.add("미정");
                         list.add("공부");
                         list.add("과제");
+                        list.add("운동");
                     }else {
                         for (DataSnapshot childData : snapshot.getChildren()) {
                             categoryDB currData = childData.getValue(categoryDB.class);
@@ -145,11 +162,16 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
             ArrayAdapter<CharSequence> adapterArray = ArrayAdapter.createFromResource(v.getContext(), R.array.category_list, android.R.layout.simple_spinner_item);
             adapterArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(adapterArray);
+            cal = Calendar.getInstance();
+
+            // layout과 button 연결
             bt_cancel = v.findViewById(R.id.bt_cancel);
             bt_listUp = v.findViewById(R.id.bt_listUp);
             content = v.findViewById(R.id.add_todo);
-            cal = Calendar.getInstance();
             delBtn = v.findViewById(R.id.delBtn);
+            timeGroup = v.findViewById(R.id.time_group);
+
+            // 삭제 버튼 구현
             delBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -157,13 +179,13 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
                     dismiss();
                 }
             });
-            todolist = v.findViewById(R.id.todolist);
-            timeGroup = v.findViewById(R.id.time_group);
+
+            // 구현해둔 OnClick 함수 연결
             bt_cancel.setOnClickListener(this);
-            // 기존의 것 수정하기.
             bt_listUp.setOnClickListener(this);
             setCancelable(false);
 
+            // 기존 할 일 수정 시 timeline 정보를 받아와 해당 timeGroup에 체크해줌
             switch (getArguments().getInt("timeline")){
                 case 0:
                     timeGroup.check(R.id.time1);
@@ -175,7 +197,10 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
                     timeGroup.check(R.id.time3);
                     break;
             }
+            // 기존 할 일 수정 시 할 일 내용을 받아와 EditText에 넣어줌
             content.setText(getArguments().getString("content"));
+
+            // 기존 할 일 수정 시 category 정보를 받아와 Spinner에서 select해줌
             switch (getArguments().getString("catalog")){
                 case "미정":
                     spinner.setSelection(0);
@@ -197,10 +222,14 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_listUp: //확인 버튼을 눌렀을 때
+
+                // 입력된 할 일이 없는 경우
                 if (content.getText().toString().equals("")) {
                     Toast.makeText(v.getContext(), "할 일이 입력되지 않았습니다.", Toast.LENGTH_LONG).show();
                     break;
                 }
+
+                // 기존에 등록되어 있던 할 일을 수정한 경우
                 if(getArguments().getInt("state") == 1){
                     System.out.println(Long.toString(getArguments().getLong("dateLong")));
                     System.out.println(Integer.toString(getArguments().getInt("timeline")));
@@ -245,6 +274,7 @@ public class TodoAddedDialog extends DialogFragment implements View.OnClickListe
                     }
                 }
 
+                // 할 일 신규 등록 시
                 if (timeGroup.getCheckedRadioButtonId() == R.id.time1) {
                     dailydb = new DailyDB();
                     dailydb.createDate = cal.getTime().toString();
--- MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/Daily/firstPage.java
+++ MobileProgrammingProjectVer3-fire--master/app/src/main/java/org/tech/mobileprogrammingproject/Daily/firstPage.java
@@ -1,6 +1,5 @@
 package org.tech.mobileprogrammingproject.Daily;
 
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -35,56 +33,61 @@ import java.util.Calendar;
 import static org.tech.mobileprogrammingproject.Daily.thirdPage.changeMemo;
 import static org.tech.mobileprogrammingproject.Daily.secondPage.changeState;
 
+
+/**
+ * firstPage.java
+ * 주요 기능: 3분할 할 일 체크리스트 구현
+ *
+ * 2020.11.14 (마지막 수정)
+ * @author 김지원
+ */
+
 public class firstPage extends Fragment implements DatePickerDialog.OnDateSetListener{
 
-    /*
-1. variables :
-    bt_date : 날짜 출력 및 DatePickerDialog 호출 버튼
-    bt_add : 할 일 추가 버튼
-    dateDialog : DatePickerDialog
-2. functions:
-    bt_date.setOnClickListener : DatePickerDialog 호출
-    onDateSet: DatePickerDialog에서 설정한 날짜로 Text 출력 값 변경
-    bt_add.setOnClickListener: 할 일 추가 팝업 onClick 구현
- */
-    private Button bt_date;
+    // variables
     public static String dateTime;
-    private Button bt_add;
-    private DatePickerDialog dateDialog;
-    private LinearLayout showDaliyTodo;
-    private CheckBox cb;
-    // row의 index담기.
-    ArrayList<DailyDB> itemIDArrayForRow = new ArrayList<>();
-    ArrayList<Integer> itemID = new ArrayList<>();
+    private Button bt_date;     // 날짜 출력 및 DatePickerDialog 호출 버튼
+    private Button bt_add;      // 할 일 추가 버튼
+    private DatePickerDialog dateDialog;    // DatePickerDialog
+    private LinearLayout showDaliyTodo;     // 할 일 목록 동적 생성을 위한 LinearLayout
     public int idx = 1;
-    public int idxNum = 0;
 
+    // row의 index를 담기 위한 DailyDB ArrayList
+    ArrayList<DailyDB> itemIDArrayForRow = new ArrayList<>();
+
+    // firebase 연결 및 DB 변수
     DatabaseReference database = null;
-    DailyDB dailydb = null;
     DailyDB solved_db = null;
 
     SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
-
     Calendar cal;
+
     static ViewGroup rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        // firstpage의 layout 받아옴
         rootView = (ViewGroup) inflater.inflate(R.layout.firstpage, container, false);
         cal = Calendar.getInstance();
+
+        // layout과 button 연결
         bt_date = rootView.findViewById(R.id.bt_date);
+        bt_add = rootView.findViewById(R.id.bt_add);
         showDaliyTodo = rootView.findViewById(R.id.todolist);
-        //firebase 연결.
+
+        // firebase 연결
         database = FirebaseDatabase.getInstance().getReference();
 
-
-        // 어플 구동 시 초기 값 지정
+        // 어플 구동 시 firstpage에 표시될 날짜 초기 값 지정
         bt_date.setText((cal.get(Calendar.MONTH)+1) + "월 " + cal.get(Calendar.DATE) + "일");
 
+        // dateDialog 작동 시 나타날 초기 날짜 설정
         dateDialog = new DatePickerDialog(getContext(), this,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH));
 
+        // setOnClickListner를 통해 bt_date를 클릭할 경우 DatePickerDialog 호출
         bt_date.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -92,15 +95,14 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
             }
         });
 
-
-        bt_add = rootView.findViewById(R.id.bt_add);
+        // setOnClickListner를 통해 bt_add 클릭할 경우 TodoAddedDialog(할 일 추가) 팝업 호출
         bt_add.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //https://stackoverflow.com/questions/15459209/passing-argument-to-dialogfragment
-                //fragment에 값 넣는 방법 intent에서 putExtra기능.
                 TodoAddedDialog e = TodoAddedDialog.getInstance();
 
+                // 날짜 정보를 bundle에 넣어 TodoAddedDialog로 보냄
+                // 오늘 날짜에 할 일이 생성될 수 있도록
                 Bundle args = new Bundle();
                 args.putInt("state",0);
                 args.putInt("year",dateDialog.getDatePicker().getYear());
@@ -113,18 +115,17 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
         });
         setting();
 
-
         dateTime = Integer.toString(cal.get(Calendar.YEAR) * 1000 + (cal.get(Calendar.MONTH) + 1) * 100 + cal.get(Calendar.DAY_OF_MONTH));
         changeState(dateTime);
         return rootView;
 
     }
 
-
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         bt_date.setText((month+1) + "월 " + dayOfMonth + "일");
 
+        // daily 하단 DB 정보에 변동이 생길 때마다 호출
         database.child("daily").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -133,13 +134,25 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                 dateTime = Integer.toString(dateDialog.getDatePicker().getYear() * 1000 + (dateDialog.getDatePicker().getMonth() + 1) * 100 + dateDialog.getDatePicker().getDayOfMonth());
                 DataSnapshot today = snapshot.child(dateTime);
                 idx = 0;
+
+
                 for (int i = 0; i < 4; i++) {
+                    /*
+                    i = 0 : 아침(radioButton01)
+                    i = 1 : 점심(radioButton02)
+                    i = 2 : 저녁(radioButton03)
+                    i = 3 : 완료된 할 일
+                    */
                     DataSnapshot currData = today.child(Integer.toString(i));
                     for (DataSnapshot childData : currData.getChildren()) {
                         DailyDB currDailyDB = childData.getValue(DailyDB.class);
+
+                        // 할 일을 담기 위한 LinearLayout 생성
                         LinearLayout tr = new LinearLayout(getActivity());
                         tr.setOrientation(LinearLayout.HORIZONTAL);
 
+                        // 3분할 정보를 나타내주는 textView1 생성
+                        // 아침, 점심, 저녁에 따라 색상을 다르게 설정함 (아침에서 저녁까지 점점 더 색이 짙어짐)
                         TextView textView1 = new TextView(getContext());
                         if(i == 0) textView1.setBackgroundColor(Color.parseColor("#ccdeeb"));
                         else if(i == 1) textView1.setBackgroundColor(Color.parseColor("#B4D3E7"));
@@ -151,6 +164,7 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         params1.weight = 0.15f;
                         textView1.setLayoutParams(params1);
 
+                        // 할 일의 내용을 담는 textView2 생성
                         TextView textView2 = new TextView(getContext());
                         textView2.setText(currDailyDB.content);
                         textView2.setGravity(Gravity.CENTER);
@@ -159,6 +173,7 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         params2.weight = 0.7f;
                         textView2.setLayoutParams(params2);
 
+                        // 할 일의 완료 여부를 체크하는 checkBox 생성
                         CheckBox cb = new CheckBox(getContext());
                         cb.setGravity(Gravity.CENTER);
                         cb.setPadding(3,10,3,10);
@@ -166,14 +181,16 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         params3.weight = 0.15f;
                         cb.setLayoutParams(params3);
 
-                        // 완료된 할 일 시각적으로 표시
+                        // 완료된 할 일 시각적으로 표시 (컬러가 아닌 회색조로 표현)
                         if(i==3) {
                             tr.setBackgroundColor(Color.parseColor("#ededed"));
                             textView1.setBackgroundColor(Color.parseColor("#BDBDBD"));
                             textView2.setTextColor(Color.parseColor("#9b9b9b"));
+                            // 완료된 상태에서는 check가 유지되도록 함
                             cb.setChecked(true);
                         } else cb.setChecked(false);
 
+                        // 할 일을 담기위해 생성해둔 LinearLayout에 내용을 담음
                         tr.addView(textView1);
                         tr.addView(textView2);
                         tr.addView(cb);
@@ -181,8 +198,11 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         tr.setId(idx++);
                         itemIDArrayForRow.add(currDailyDB);
 
+                        // 수정을 위해 할 일 LinearLayout을 클릭이 가능하도록 설정
                         tr.setClickable(true);
 
+                        // 클릭 시에 DB에 담아두었던 정보를 받아와 bundle을 통해 TodoAddedDialog로 보냄
+                        // TodoAddedDialog 팝업 시에 기존 정보가 저장된 채로 팝업되게 하기 위함 (for 할 일 수정)
                         tr.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -203,33 +223,15 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                             }
                         });
 
-                        tr.setOnLongClickListener(new View.OnLongClickListener() {
-                            @Override
-                            public boolean onLongClick(View v) {
-                                int i = rootView.getId();
-                                DailyDB solve = itemIDArrayForRow.get(i+1);
-
-                                solved_db = new DailyDB();
-                                solved_db.createDate = solve.createDate;
-                                solved_db.content = solve.content;
-                                solved_db.state = 0;
-                                solved_db.timeline = solve.timeline;
-                                solved_db.catalog = solve.catalog;
-                                solved_db.date = solve.date;
-
-                                database.child("daily").child(Long.toString(solved_db.date)).child("3").child(solved_db.createDate).removeValue();
-
-                                database.child("daily").child(Long.toString(solved_db.date)).child(String.valueOf(solve.timeline)).child(solve.createDate).removeValue();
-                                database.child("daily").child(Long.toString(solved_db.date)).child(String.valueOf(solve.timeline)).child(solve.createDate).setValue(solved_db);
-
-                                return true;
-                            }
-                        });
-
+                        /* CheckBox 체크 시 해당 할 일 DB를 database.child("daily").child("할 일이 작성된 날짜").child("3")으로 이동
+                        기존의 DB 위치를 체크하여 삭제하고, Status 팝업에서 시간 정보까지 받아 새로운 위치에서 재생성되도록 구현함
+                         */
                         cb.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Status s = Status.getInstance();
+
+                                // Status 팝업에서 시간정보를 업데이트 해야하므로 번들에 정보를 넣어 전송해줌
                                 Bundle bundle = new Bundle();
                                 int i = rootView.getId();
                                 DailyDB solve = itemIDArrayForRow.get(i+1);
@@ -243,6 +245,8 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
 
                                 s.setArguments(bundle);
 
+                                // 할 일 DB 위치 이동을 위해 기존 DB 내용을 받아와
+                                // 위치와 timeline 정보를 업데이트해서 setValue로 새 위치에 넣어줌
                                 solved_db = new DailyDB();
                                 solved_db.createDate = solve.createDate;
                                 solved_db.content = solve.content;
@@ -259,6 +263,8 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                                 s.show(getChildFragmentManager(), Status.TAG_STATUS_DIALOG);
                             }
                         });
+
+                        // 할 일이 완료되었을 경우 완료된 할 일이 담긴 LinearLayout과 CheckBox가 클릭되지 않도록 설정함
                         if(currDailyDB.timeline == 3) {
                             tr.setEnabled(false);
                             cb.setEnabled(false);
@@ -266,11 +272,11 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         showDaliyTodo.addView(tr);
                     }
                 }
+
                 // 2020.11.01 황교민
                 // 사용자가 선택한 날짜를 공유하는 static 변수인 dateTime이 변경되었을 경우, fragment에서는 변화를 인식하지 못함.
                 // changeMemo메소드를 import하여 memo의 내용을 수정하도록 함.
                 changeMemo(dateTime);
-
                 changeState(dateTime);
             }
 
@@ -282,23 +288,37 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
         });
     }
 
+    // firstPage에 처음 접속 시 화면에 할 일이 표시되지 않는 기능을 보완하기 위해 함수를 생성함
+    // 코드 내용은 위와 같음
     public void setting(){
+
+        // daily 하단 DB 정보에 변동이 생길 때마다 호출
         database.child("daily").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 itemIDArrayForRow.clear();
-                itemID.clear();
                 idx = 0;
                 showDaliyTodo.removeViews(1,showDaliyTodo.getChildCount() - 1);
                 final String currDate = Long.toString(cal.get(Calendar.YEAR) * 1000 + (cal.get(Calendar.MONTH) + 1) * 100 + cal.get(Calendar.DATE));
                 DataSnapshot today = snapshot.child(currDate);
+
                 for (int i = 0; i < 4; i++) {
+                    /*
+                    i = 0 : 아침(radioButton01)
+                    i = 1 : 점심(radioButton02)
+                    i = 2 : 저녁(radioButton03)
+                    i = 3 : 완료된 할 일
+                    */
                     DataSnapshot currData = today.child(Integer.toString(i));
                     for (DataSnapshot childData : currData.getChildren()) {
                         DailyDB currDailyDB = childData.getValue(DailyDB.class);
+
+                        // 할 일을 담기 위한 LinearLayout 생성
                         LinearLayout tr = new LinearLayout(getActivity());
                         tr.setOrientation(LinearLayout.HORIZONTAL);
 
+                        // 3분할 정보를 나타내주는 textView1 생성
+                        // 아침, 점심, 저녁에 따라 색상을 다르게 설정함 (아침에서 저녁까지 점점 더 색이 짙어짐)
                         TextView textView1 = new TextView(getContext());
                         if(i == 0) textView1.setBackgroundColor(Color.parseColor("#ccdeeb"));
                         else if(i == 1) textView1.setBackgroundColor(Color.parseColor("#B4D3E7"));
@@ -310,6 +330,7 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         params1.weight = 0.15f;
                         textView1.setLayoutParams(params1);
 
+                        // 할 일의 내용을 담는 textView2 생성
                         TextView textView2 = new TextView(getContext());
                         textView2.setText(currDailyDB.content);
                         textView2.setGravity(Gravity.CENTER);
@@ -318,6 +339,7 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         params2.weight = 0.7f;
                         textView2.setLayoutParams(params2);
 
+                        // 할 일의 완료 여부를 체크하는 checkBox 생성
                         CheckBox cb = new CheckBox(getContext());
                         cb.setGravity(Gravity.CENTER);
                         cb.setPadding(3,10,3,10);
@@ -326,8 +348,12 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                         cb.setLayoutParams(params3);
                         tr.setId(idx++);
                         itemIDArrayForRow.add(currDailyDB);
+
+                        // 수정을 위해 할 일 LinearLayout을 클릭이 가능하도록 설정
                         tr.setClickable(true);
 
+                        // 클릭 시에 DB에 담아두었던 정보를 받아와 bundle을 통해 TodoAddedDialog로 보냄
+                        // TodoAddedDialog 팝업 시에 기존 정보가 저장된 채로 팝업되게 하기 위함 (for 할 일 수정)
                         tr.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -347,10 +373,15 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                             }
                         });
 
+                        /* CheckBox 체크 시 해당 할 일 DB를 database.child("daily").child("할 일이 작성된 날짜").child("3")으로 이동
+                        기존의 DB 위치를 체크하여 삭제하고, Status 팝업에서 시간 정보까지 받아 새로운 위치에서 재생성되도록 구현함
+                         */
                         cb.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Status s = Status.getInstance();
+
+                                // Status 팝업에서 시간정보를 업데이트 해야하므로 번들에 정보를 넣어 전송해줌
                                 Bundle bundle = new Bundle();
                                 int i = rootView.getId();
                                 DailyDB solve = itemIDArrayForRow.get(i+1);
@@ -368,21 +399,22 @@ public class firstPage extends Fragment implements DatePickerDialog.OnDateSetLis
                                 v.setEnabled(true);
                             }
                         });
+
+                        // 할 일이 완료되었을 경우 완료된 할 일이 담긴 LinearLayout과 CheckBox가 클릭되지 않도록 설정함
                         if(currDailyDB.timeline == 3) {
                             tr.setEnabled(false);
                             cb.setEnabled(false);
                         }
-                        int id = rootView.getId();
-                        DailyDB solve = itemIDArrayForRow.get(id+1);
+
+                        // 완료된 할 일 시각적으로 표시 (컬러가 아닌 회색조로 표현)
                         if(i==3) {
                             tr.setBackgroundColor(Color.parseColor("#ededed"));
                             textView1.setBackgroundColor(Color.parseColor("#BDBDBD"));
                             textView2.setTextColor(Color.parseColor("#9b9b9b"));
                             cb.setChecked(true);
-                        } else if(database.child("daily").child(Long.toString(solve.date)).child("3").child(solve.createDate) == null){
-                            cb.setChecked(false);
-                        }
+                        } else cb.setChecked(false);
 
+                        // 할 일을 담기위해 생성해둔 LinearLayout에 내용을 담음
                         tr.addView(textView1);
                         tr.addView(textView2);
                         tr.addView(cb);
